aecf8d709cb820a05280be85abd831bb2205cc2c
arty <arty@sfu.ca>
2023-05-21 06:28:14 -0700

    convinience of life functions

--- src/components/room-page/chat-panel/chatgptSetup.js
+++ src/components/room-page/chat-panel/chatgptSetup.js
@@ -9,12 +9,28 @@ const configuration = new Configuration({
 delete configuration.baseOptions.headers['User-Agent'];
 
 const openai = new OpenAIApi(configuration);
+var persona = ""
+
+export function setPersona(phrase) {
+    persona = phrase.trim();
+}
+
+function createPrompt(message, codeText, codeLang) {
+    prompt = `For the following code in ${codeLang}: \`\`\`{codeText}\`\`\`` +
+        `Answer the question: ${message}`;
+
+    if (persona.length > 0) {
+        prompt = `Please assume the persona of ${persona}` + prompt;
+    }
+
+    return prompt;
+}
 
 // retrieve the response from chatgpt api and set it in a callback
-export const getGPTResponse = async (prompt, setResponse) => {
+export const getGPTResponse = async (message, codeText, codeLang, setResponse) => {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: prompt,
+    prompt: {createPrompt(message, codeText, codeLang)},
     max_tokens: 2000,
   });
 
