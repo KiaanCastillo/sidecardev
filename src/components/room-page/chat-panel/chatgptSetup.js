57e96604975715f8dd88d749e0ca27ace0653c04
talzaloilov <tzaloilo@sfu.ca>
2023-05-21 09:29:28 -0700

    Moved chatgpt functionality to backend

--- src/components/room-page/chat-panel/chatgptSetup.js
+++ src/components/room-page/chat-panel/chatgptSetup.js
@@ -1,46 +1,58 @@
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-});
+import axios from 'axios';
 
-// remove 'User-Agent' header to bypass openai restriction on client application
-delete configuration.baseOptions.headers["User-Agent"];
+// const configuration = new Configuration({
+//   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(configuration);
-var persona = "";
+// // remove 'User-Agent' header to bypass openai restriction on client application
+// delete configuration.baseOptions.headers["User-Agent"];
 
-export function setPersona(phrase) {
-  persona = phrase.trim();
+// const openai = new OpenAIApi(configuration);
+// var persona = "";
+
+// export function setPersona(phrase) {
+//   persona = phrase.trim();
+// }
+
+// function createPrompt(message, codeText, codeLang) {
+//   let prompt = `${message}.
+//     If applicable, here is some code for context written in ${codeLang}: \`\`\`${codeText}\`\`\``;
+
+//   if (persona.length > 0) {
+//     prompt = `Please assume the persona of ${persona}` + prompt;
+//   }
+
+//   return prompt;
+// }
+
+// TODO = change back
+
+// // retrieve the response from chatgpt api and set it in a callback
+// export const getGPTResponse = async (
+//   message,
+//   codeText,
+//   codeLang,
+//   setResponse
+// ) => {
+//   const prompt = createPrompt(message, codeText, codeLang);
+//   const response = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: prompt,
+//     max_tokens: 500,
+//   });
+
+//   if (response.data.choices) {
+//     setResponse(response.data.choices[0].text);
+//     return response.data.choices[0].text;
+//   }
+// };
+
+export const getGPTResponse = async (message, codeText, codeLang, setResponse) => {
+    // const prompt = createPrompt(message, codeText, codeLang);
+    const response = await axios.get('http://localhost:3000/chatgpt/getGPTResponse', { params: {msg: message, codeTxt: codeText, codeLng: codeLang, setRespnse : setResponse} });
+
+    console.log(response);
+    return response.data;
 }
-
-function createPrompt(message, codeText, codeLang) {
-  let prompt = `${message}.
-    If applicable, here is some code for context written in ${codeLang}: \`\`\`${codeText}\`\`\``;
-
-  if (persona.length > 0) {
-    prompt = `Please assume the persona of ${persona}` + prompt;
-  }
-
-  return prompt;
-}
-
-// retrieve the response from chatgpt api and set it in a callback
-export const getGPTResponse = async (
-  message,
-  codeText,
-  codeLang,
-  setResponse
-) => {
-  const prompt = createPrompt(message, codeText, codeLang);
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: prompt,
-    max_tokens: 500,
-  });
-
-  if (response.data.choices) {
-    setResponse(response.data.choices[0].text);
-    return response.data.choices[0].text;
-  }
-};
