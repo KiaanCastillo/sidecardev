aa27de1fa043de36bae0989aaa3c50b071fd8568
Kiaan Castillo <kiaan_castillo@sfu.ca>
2023-05-21 02:01:11 -0700

    Add tool tips to user avatar bubbles

--- src/components/room-page/header-nav/index.jsx
+++ src/components/room-page/header-nav/index.jsx
@@ -1,15 +1,19 @@
 import { Logo } from "./Logo";
 import { Button } from "../../button";
 import { IconLink, IconQRCode } from "../../icon";
+import { ChatGPTUserIcon } from "./chat-gpt-user-icon";
 
 import styles from "./styles.module.scss";
+import { useState } from "react";
 
 export const HeaderNav = () => {
   return (
     <header className={styles.header}>
       <Logo />
-      <UserAvatarList users={["Monica", "Ross", "Joey"]} />
       <div className={styles.actions}>
+        <UserAvatarList
+          users={["Monica", "Ross", "Rachel", "Phoebe", "Joey", "Chandler"]}
+        />
         <Button variant="secondary">
           <IconQRCode />
           Show QR Code
@@ -24,13 +28,51 @@ export const HeaderNav = () => {
 };
 
 const UserAvatarList = ({ users = [] }) => {
+  const [currentUserToolTip, setCurrentUserToolTip] = useState("");
+
   return (
     <div className={styles.userAvatarList}>
       {users.map((name, index) => (
-        <div className={styles.avatar} key={index}>
-          {name[0]}
+        <div
+          className={styles.avatarContainer}
+          key={index}
+          onMouseEnter={() => setCurrentUserToolTip(name)}
+          onMouseLeave={() => setCurrentUserToolTip("")}
+        >
+          <div className={styles.avatar}>{name[0]}</div>
+          <span
+            className={`${styles.toolTip} ${
+              currentUserToolTip === name ? styles.showToolTip : ""
+            }`}
+          >
+            {name}
+          </span>
         </div>
       ))}
+      <ChatGPTUserAvatar />
+    </div>
+  );
+};
+
+const ChatGPTUserAvatar = () => {
+  const [showToolTip, setShowToolTip] = useState(false);
+
+  return (
+    <div
+      className={styles.avatarContainer}
+      onMouseEnter={() => setShowToolTip(true)}
+      onMouseLeave={() => setShowToolTip(false)}
+    >
+      <div className={styles.chatGptUserAvatar}>
+        <ChatGPTUserIcon />
+        <span
+          className={`${styles.toolTip} ${
+            showToolTip ? styles.showToolTip : ""
+          }`}
+        >
+          ChatGPT
+        </span>
+      </div>
     </div>
   );
 };
